0b83c93c08a496028b3a295c564a0927d87b0f93
Artenis67 <artenis67eininger@gmail.com>
2023-07-01 18:45:22 +0200

    Installation de l'app Flask

---
+++ Python/get_a_quote.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Endpoint de l'API
+    url = "https://quotes.rest/qod"
+
+    # Paramètres de requête (incluant la clé d'API)
+    params = {
+        "language": "fr",  # Langue des citations (français)
+        "category": "inspire",  # Catégorie des citations (inspirantes)
+        "api_key": "jcLYnSzb1FReLe9LM23SVLxbFPMs5KNTX6MvFGQH"  # Remplacez par votre propre clé d'API
+    }
+
+    # Faire la requête à l'API avec les paramètres
+    response = requests.get(url, params=params)
+
+    # Vérifier si la requête a réussi (code 200 signifie succès)
+    if response.status_code == 200:
+        # Extraire les données de la réponse
+        data = response.json()
+
+        # Vérifier si la clé 'quotes' est présente dans la réponse
+        if "quotes" in data["contents"]:
+            # Récupérer la citation aléatoire
+            quote = data["contents"]["quotes"][0]["quote"]
+            author = data["contents"]["quotes"][0]["author"]
+
+            # Afficher la citation et l'auteur
+            print(f"{quote} - {author}")
+        else:
+            print("La clé 'quotes' n'est pas présente dans la réponse de l'API.")
+    else:
+        print("Erreur lors de la requête à l'API.")
+
+    result = "Résultat de l'exécution du code Python"
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run()
